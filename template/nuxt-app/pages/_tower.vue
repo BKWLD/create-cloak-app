2d89e5ae1b4bacc5a124778c11cef7d214bf3c61
Robert Reinhard <robert.reinhard@bukwild.com>
2021-08-27 14:40:46 -0700

    Add Contentful support to Tower

--- template/nuxt-app/pages/_tower.vue
+++ template/nuxt-app/pages/_tower.vue
@@ -9,7 +9,11 @@
 <!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
 <script lang='coffee'>
+<%_ if (cms == 'craft') { _%>
 import getCraftPages from '~/queries/craft-pages.gql'
+<%_ } else if (cms == 'contentful') { _%>
+import getTower from '~/queries/tower.gql'
+<%_ } _%>
 import pageMixin from '@bkwld/cloak/mixins/page'
 
 export default
@@ -24,12 +28,18 @@ export default
 		return app.$notFound() if params.tower in [ '__webpack_hmr' ]
 
 		# Get data
+		<%_ if (cms == 'craft') { _%>
 		[ page ] = payload || await app.$craft.getEntries
 			query: getCraftPages
 			variables:
 				section: 'towers'
 				type: 'towers'
 				uri: params.tower || '__home__'
+		<%_ } else if (cms == 'contentful') { _%>
+		page = payload || await app.$contentful.getEntry
+			query: getTower
+			variables: route: params.tower || '/'
+		<%_ } _%>
 		return app.$notFound() unless page
 
 		# Set data
