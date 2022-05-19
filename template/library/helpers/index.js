2c62055aef7b1647b30a97ef7fd74a7775899f3d
Robert Reinhard <robert.reinhard@bukwild.com>
2022-05-19 11:43:42 -0700

    Revert "Satisfying js warning about no default export"
    This reverts commit 4abada3a25e75c53f4b37c5e19d7432464acfdde.

--- template/library/helpers/index.js
+++ template/library/helpers/index.js
@@ -6,7 +6,3 @@ export * from './dom'
 export * from './formatting'
 export * from './routing'
 export * from './storage'
-
-// Without this, shopify-theme emitted a warning of:
-// "export 'default' (imported as 'projectHelpers') was not found
-export default {}
