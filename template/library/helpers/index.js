4abada3a25e75c53f4b37c5e19d7432464acfdde
Robert Reinhard <robert.reinhard@bukwild.com>
2022-05-19 08:30:44 -0700

    Satisfying js warning about no default export

--- template/library/helpers/index.js
+++ template/library/helpers/index.js
@@ -6,3 +6,7 @@ export * from './dom'
 export * from './formatting'
 export * from './routing'
 export * from './storage'
+
+// Without this, shopify-theme emitted a warning of:
+// "export 'default' (imported as 'projectHelpers') was not found
+export default {}
