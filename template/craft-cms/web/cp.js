ce02c3dfdfe841b7d6a80ab2b415ac0c8d710311
Robert Reinhard <robert.reinhard@bukwild.com>
2022-06-15 08:02:12 -0700

    Refactoring live preview JS

--- template/craft-cms/web/cp.js
+++ template/craft-cms/web/cp.js
@@ -1,7 +1,6 @@
 // Fire postMessage as part of live preview
 // https://github.com/craftcms/cms/issues/5359
 Garnish.on(Craft.Preview, 'beforeUpdateIframe', function(event) {
-	if (!event.refresh) {
-		event.target.$iframe[0].contentWindow.postMessage('preview:change', '*');
-	}
-});
+	if (event.refresh) return
+	event.target.$iframe[0].contentWindow.postMessage('preview:change', '*')
+})
